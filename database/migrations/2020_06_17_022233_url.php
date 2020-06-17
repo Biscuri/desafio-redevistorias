2d84f7f51da8c5a352bd615745c402e8a036ff9d
Leno <leno@linceweb.com.br>
2020-06-17 01:31:32 -0300

    Fix migrate url

--- database/migrations/2020_06_17_022233_url.php
+++ database/migrations/2020_06_17_022233_url.php
@@ -14,7 +14,7 @@ class Url extends Migration
     public function up()
     {
         Schema::create('urls', function (Blueprint $table) {
-            $table->id();
+            $table->string('id')->primary();
             $table->unsignedBigInteger('hits')->default(0);
             $table->string('shortUrl')->unique();
             $table->string('url')->unique();
